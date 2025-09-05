a340344f1be868c6448a60a117bfc23c58da9d3e
Nat0993 <natasha.serebryak@yandex.ru>
2025-09-05 16:15:21 +0500

    создание простого роутера

---
+++ frontend/src/js/router.js
@@ -0,0 +1,59 @@
+class Router {
+    constructor() {
+        this.routes = {}; //объект для хранения маршрутов ('путь': функция)
+        this.currentPage = null;// хранит текущую страницу
+        console.log('Роутер создан');
+    }
+
+    //метод добавления маршрута
+    addRouter(page, renderFunction) {
+        this.routes[path] = renderFunction;
+        console.log(`Добавлен маршрут ${path}`);
+    }
+
+    //переход на страницу
+    navigateTo(path) {
+        console.log(`Переходим на ${path}`);
+        window.history.pushState({}, '', path);
+        this.loadPage(path);
+    }
+
+    // Загрузка страницы
+    loadPage(path) {
+        const renderFunction = this.routes[path] || this.routes['/404'];
+
+        if (renderFunction) {
+            renderFunction();
+        } else {
+            console.error('Маршрут не найден:', path);
+            this.show404();
+        }
+    }
+
+    show404() {
+        document.getElementById('app').innerHTML = `
+            <div style="padding: 20px; text-align: center;">
+                <h1>404</h1>
+                <p>Страница не найдена</p>
+                <button onclick="router.navigateTo('/')">
+                    На главную
+                </button>
+            </div>
+        `;
+    }
+
+    init() {
+        console.log('Роутер запущен');
+
+        // Обработка кнопок "назад/вперед"
+        window.addEventListener('popstate', () => {
+            console.log('Нажата кнопка назад/вперед');
+            this.loadPage(window.location.pathname);
+        })
+
+        // Загрузка текущей страницы
+        this.loadPage(window.location.pathname);
+    }
+}
+
+export const router = new Router();
