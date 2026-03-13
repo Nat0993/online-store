90185fe490d0e486b6a2971bd9ec1c29d5f22eaa
Nat0993 <natasha.serebryak@yandex.ru>
2026-03-13 16:24:43 +0500

    feat: создан vue-компонент pageHeader

---
+++ vue-version/src/components/PageHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="page-header">
+        <h1 class="page-header__title">{{ title }}</h1>
+        <span class="page-header__description">{{ description }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    title: string;
+    description: string;
+}>();
+</script>
