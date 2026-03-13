54e024a5e9ee532f69e09bb729a156a351cb8e1d
Nat0993 <natasha.serebryak@yandex.ru>
2026-03-13 16:31:30 +0500

    feat: создан vue-компонент EmptyMessage

---
+++ vue-version/src/components/EmptyMessage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="empty-message">
+        <div class="page-header">
+            <h3 class="page-header__title">{{ title }}</h3>
+            <span class="page-header__description">{{ description }}</span>
+        </div>
+        <a 
+            :href="link.href" 
+            class="empty-message__btn btn"
+            @click.prevent="navigate"
+        >
+            {{ link.label }}
+        </a>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { NavLink } from '../types/index.ts';
+
+const props = defineProps<{
+title: string;
+description: string;
+link: NavLink;
+}>()
+
+const navigate = () => {
+    window.history.pushState({}, '', props.link.href);
+    window.dispatchEvent(new PopStateEvent('popstate'));
+}
+</script>
